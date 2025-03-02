9a68e3baa19607b1710e6403fc7226ecb10522cb
Pranav Doshetty <pranav.doshetty@gmail.com>
2025-03-01 16:18:20 -0800

    modified examples to get token from file not env, store data retrieved to csv file, set headless==False, set hashtag and rows as variables

--- examples/hashtag_example.py
+++ examples/hashtag_example.py
@@ -1,17 +1,35 @@
 from TikTokApi import TikTokApi
 import asyncio
 import os
+import csv
+import json
 
-ms_token = os.environ.get("ms_token", None)  # set your own ms_token
+with open('secrets/secrets.json') as f:
+    secrets = json.load(f)
+
+ms_token = secrets.get("ms_token", None)  # set your own ms_token
 
 
 async def get_hashtag_videos():
     async with TikTokApi() as api:
-        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, browser=os.getenv("TIKTOK_BROWSER", "chromium"))
-        tag = api.hashtag(name="funny")
-        async for video in tag.videos(count=30):
-            print(video)
-            print(video.as_dict)
+        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, browser=os.getenv("TIKTOK_BROWSER", "chromium"), headless=False)
+        hashtag = "makeup"
+        video_count = 1000
+        tag = api.hashtag(name=hashtag)
+        async for video in tag.videos(count=video_count):
+            # print(video)
+            # Convert video to dictionary
+            video_dict = video.as_dict
+
+            # Write to CSV file
+            with open(f'data/videos_{hashtag}_{video_count}.csv', mode='a', newline='') as csv_file:
+                fieldnames = video_dict.keys()
+                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                if csv_file.tell() == 0:
+                    writer.writeheader()
+                writer.writerow(video_dict)
+
+        print(f"stored video for hashtag {hashtag} with total rows {video_count}")
 
 
 if __name__ == "__main__":
--- examples/trending_example.py
+++ examples/trending_example.py
@@ -1,13 +1,17 @@
 from TikTokApi import TikTokApi
 import asyncio
 import os
+import json
 
-ms_token = os.environ.get("ms_token", None)  # set your own ms_token
+with open('secrets/secrets.json') as f:
+    secrets = json.load(f)
+
+ms_token = secrets.get("ms_token", None)  # set your own ms_token
 
 
 async def trending_videos():
     async with TikTokApi() as api:
-        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, browser=os.getenv("TIKTOK_BROWSER", "chromium"))
+        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, browser=os.getenv("TIKTOK_BROWSER", "chromium"), headless=False)
         async for video in api.trending.videos(count=30):
             print(video)
             print(video.as_dict)
